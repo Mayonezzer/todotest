eebc841ea5a1d02194b1c25488d4610e02737c5c
Mayonezzer <therullfetard@gmail.com>
2023-02-14 19:37:36 +0300

    14.02 filter/useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./TodoList";
 import {TaskPropsType} from "./TodoList";
 
 export function App() {
     const todoListTitle: string = "what to learn"
-    let tasksList: Array<TaskPropsType> = [
+    let [tasksList, setTasksList] = useState<Array<TaskPropsType>>( [
         {taskId: 1, taskTitle: "html&css", isDone: true},
         {taskId: 2, taskTitle: "JS", isDone: true},
         {taskId: 3, taskTitle: "notJS", isDone: true},
         {taskId: 4, taskTitle: "ReactJS", isDone: false},
-    ]
+    ])
 
-    const removeTask = (id: number, myTitle: string) => {
+    let removeTask = (id: number, myTitle: string) => {
         tasksList = tasksList.filter((el) => el.taskId !== id)
+        setTasksList(tasksList)
     }
 
     return (
